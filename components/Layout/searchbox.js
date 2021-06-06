ef66221a20a35a4d2e7583a7644e77aab286b993
NicoleStrunga <nicole.strungova@gmail.com>
2021-06-06 11:22:17 +0200

    Searchbox page and component base.

---
+++ components/Layout/searchbox.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../data/listB.json';
+import { Searchbox } from '@appbaseio/reactivesearch';
+import DataSearch from 'next/link';
+
+const Searchbox = () => {
+  return <div>searchbox</div>;
+};
+
+export default Searchbox;
---
+++ pages/search.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Searchbox } from '../components/Layout/searchbox';
+
+const SearchPage = () => {
+  return <Searchbox />;
+};
+
+export default SearchPage;
