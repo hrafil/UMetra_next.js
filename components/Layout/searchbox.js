4d934a8e3161390daabc4da8a43cbc220317c2fb
NicoleStrunga <nicole.strungova@gmail.com>
2021-06-06 16:52:24 +0200

    Vytvořené pole artworksIn kvůli vyhledávání podle ostatních kategorii.

--- components/Layout/searchbox.js
+++ components/Layout/searchbox.js
@@ -3,32 +3,49 @@ import Search from 'react-search';
 import lineB from '../../data/listB.json';
 
 const searchField = () => {
-  const [station, setStation] = useState();
+  const [searchInput, setInput] = useState();
 
   const stationsFull = lineB.filter((item) => item.artworks !== false);
 
+  const artworksIn = [];
+  stationsFull.map((station) => {
+    station.artworks.map((artwork) => artworksIn.push(artwork));
+  });
+  console.log(artworksIn);
+
   const searchStation = () =>
     stationsFull.find((item) => {
-      if (item.station === station) {
+      if (item.station === searchInput) {
         return item.station;
       }
     });
-  console.log(searchStation());
+  // console.log(searchStation());
+
+  const searchName = () =>
+    stationsFull.find((station) => {
+      if (station.name === searchInput) {
+        return station.name;
+      }
+    });
+  // console.log(searchName());
+
+  const searchType = () =>
+    stationsFull.find((station) => {
+      if (station.type === searchInput) {
+        return station.type;
+      }
+    });
+  // console.log(searchType());
 
   return (
     <>
       <label>
         Hledám...
-        <input type="text" onChange={(e) => setStation(e.target.value)} />
+        <input type="text" onChange={(e) => setInput(e.target.value)} />
       </label>
     </>
   );
 };
-
-// export const stationsFull = lineB.filter(
-//   (station) => station.artworks !== false,
-// );
-// console.log(stationsFull);
 
 export const SearchStation = ({ stationsFull }) => {
   return (
@@ -40,12 +57,31 @@ export const SearchStation = ({ stationsFull }) => {
   );
 };
 
-export const getStaticProps = () => {
+// export const getStaticProps = () => {
+//   const stationBList = getStation();
+//   return {
+//     props: {
+//       lines: stationBList,
+//     },
+//   };
+// };
+
+export const getStaticProps = ({ params }) => {
   const stationBList = getStation();
   return {
     props: {
-      lines: stationBList,
+      station: stationBList.find((station) => station.station === params.art),
     },
+  };
+};
+
+export const getStaticPaths = () => {
+  const stationBList = getStation();
+  return {
+    paths: stationBList.map((station) => ({
+      params: { art: station.station },
+    })),
+    fallback: false,
   };
 };
 
