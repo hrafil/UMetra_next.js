bb428d0718d99d0dacbd140c39afc362951af6ea
Markéta Hornerová <hornerova.m@gmail.com>
2021-06-06 13:40:20 +0200

    neaktivni kde nic neni

--- pages/line-b.js
+++ pages/line-b.js
@@ -1,26 +1,26 @@
 import Link from 'next/link';
 import { getStation } from '../lib/stationB';
-import styles from '../styles/Lines.module.css';
+// import styles from '../styles/Lines.module.css';
 import Modal from 'react-modal';
 import { useRouter } from 'next/router';
 import { Artwork } from './line-b/[art]';
 
-Modal.setAppElement('#__next');
+// Modal.setAppElement('#__next');
 
 const Line = ({ line }) => {
   const router = useRouter();
 
+  const lineClass = !line.artworks ? 'station' : 'station yellow';
+
   return (
     <div>
-      <div className={styles.line}>
+      <div className="line">
         <Link
           href={`/line-b/[art]?art=${line.station}`}
           as={`/line-b/${line.station}`}
         >
-          <div className={styles.station}>
-            <span className={styles.label}>
-              {line.station} {line.artworks.name}
-            </span>
+          <div className={lineClass}>
+            <span className="label">{line.station}</span>
           </div>
         </Link>
       </div>
