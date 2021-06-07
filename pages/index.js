da453227206e76c44498d3bec54336e6218b1a38
Markéta Hornerová <hornerova.m@gmail.com>
2021-06-07 14:27:07 +0200

    Zaklad stylovani Home

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+// import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 
 const Home = () => {
   return (
-    <ul>
-      <li>
+    <div className="container_home">
+      <li className="btn_green">
         <Link href="/line-a">A</Link>
       </li>
-      <li>
+      <li className="btn_yellow">
         <Link href="/line-b">B</Link>
       </li>
-      <li>
+      <li className="btn_red">
         <Link href="/line-c">C</Link>
       </li>
-    </ul>
+    </div>
   );
 };
 
