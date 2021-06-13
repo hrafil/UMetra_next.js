130e29209a401e99ca1340c8046017b74e9f25b9
Markéta Hornerová <hornerova.m@gmail.com>
2021-06-13 17:00:17 +0200

    Burger pokus

--- components/Nav/RightNav.jsx
+++ components/Nav/RightNav.jsx
@@ -5,18 +5,22 @@ import Link from 'next/link';
 const Ul = styled.ul`
   list-style: none;
   display: flex;
-    flex-flow: column nowrap;
-    background-color: ##ffffff;
-    position: fixed;
-    transform: ${({ open }) => (open ? 'translateX(0)' : 'translateX(100%)')};
-    top: 0;
-    right: 0;
-    height: 100vh;
-    width: 300px;
-    padding-top: 3.5rem;
-    transition: transform 0.3s ease-in-out;
+  flex-flow: column nowrap;
+  background-color: ##ffffff;
+  position: fixed;
+  transform: ${({ open }) => (open ? 'translateX(0)' : 'translateX(100%)')};
+  top: 0;
+  right: 0;
+  height: 100vh;
+  width: 300px;
+  padding-top: 3.5rem;
+  transition: transform 0.3s ease-in-out;
+  z-index: 100,
+  width: 100vw;
+  height: 100vh;
 
-    li {
+
+  li {
       color: #fff;
     }
   }
