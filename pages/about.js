1ed676104cd2f5cf1e0096d1f3e0a4b9a0db97a7
Markéta Hornerová <hornerova.m@gmail.com>
2021-06-13 16:36:28 +0200

    Zkouska stylu

--- pages/about.js
+++ pages/about.js
@@ -7,9 +7,19 @@ const About = () => {
     <div className={styles.container}>
       <h2 className={styles.title}>o projektu</h2>
       <p className={styles.text}>
-        Interaktivní mapa metra A, B a C, kde jsou v jednotlivých stanicích
-        zaznamenána umělecká díla, která se v nich nachází. Zahrnuje malby,
-        reliéfy, sochy, mozaiky a další instalace ve veřejném prostoru.
+        Projeďte se metrem a prozkoumejte malby, reliéfy, sochy, mozaiky a další
+        instalace ve veřejném prostoru. UMetra zahrnuje nejen díla, která
+        vznikla při budování stanic, ale také nově nainstalovaná v jejich okolí.
+      </p>
+
+      <p>
+        Část z děl, která vznikala společně se stanicemi, byla již odstraněna,
+        zastavěna stánky či obklopena vizuálním smogem. Cílem tohoto projektu je
+        probudit zvědavost a zájem o umění ve veřejném prostoru, aby docházelo k
+        jeho obnově a novým instalacím spíše, než k jeho ničení.
+      </p>
+      <p>
+        Umetra vzniklo jako závěrečný projekt Digitální Akademie Web, Czechitas.
       </p>
     </div>
   );
