75c477d6db192fee49a0b7377608cc7fc6c45883
Markéta Hornerová <hornerova.m@gmail.com>
2021-06-13 18:16:19 +0200

    Burger bez zavreni

--- components/Nav/Burger.jsx
+++ components/Nav/Burger.jsx
@@ -21,7 +21,7 @@ const StyledBurger = styled.div`
   div {
     width: 2rem;
     height: 0.25rem;
-    background-color: ${({ open }) => (open ? 'black' : '#333')};
+    background-color: ${({ open }) => (open ? 'black' : '#707070')};
     border-radius: 10px;
     transform-origin: 1px;
     transition: all 0.3s linear;
